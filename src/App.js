24a4d443c5c124110128a189162c129f031473fa
Sathiya priya <97939755+gsathiya91@users.noreply.github.com>
2022-05-11 22:24:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+import HomePage from './Component/Home/home';
+
+
+
+export default function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
